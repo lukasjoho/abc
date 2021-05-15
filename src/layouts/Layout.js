ca1b0a3817ddd371dd3c18d0328c5860339a172b
lukasjoho <social@lukashoppe.com>
2021-05-15 19:51:23 +0200

    Change Layout component structure

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,18 +1,30 @@
 import * as React from "react"
 import PropTypes from "prop-types"
+import Navigation from "../components/Navigation"
 import Sidebar from "../components/Sidebar"
 import Theme from "src/Theme"
 import GlobalStyle from "src/GlobalStyle"
-
+import styled from "styled-components"
+import Header from "../components/header"
+const StyledLayout = styled.div`
+  display: flex;
+  width: 100vw;
+  min-height: 100%;
+  .grow {
+    flex-grow: 1;
+  }
+`
 const Layout = ({ children }) => {
   return (
     <>
       <Theme>
         <GlobalStyle />
-        <div>
+        <Header />
+        <StyledLayout>
+          <Navigation />
+          <div className="grow">{children}</div>
           <Sidebar />
-          {children}
-        </div>
+        </StyledLayout>
       </Theme>
     </>
   )
