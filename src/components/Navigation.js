2fcdfd5ff2695cd1a9566841f72ea0cd91ad08d8
lukasjoho <social@lukashoppe.com>
2021-05-16 11:47:06 +0200

    Create navigation menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,40 +1,105 @@
 import { Link } from "gatsby"
 import React from "react"
 import styled from "styled-components"
-
+import Emoji from "react-apple-emojis"
+import ImageAcme from "src/images/image-acmecorp.png"
 const StyledNavigation = styled.div`
   background: ${props => props.theme.colors.muted};
-  width: 24rem;
+  width: 26rem;
+  padding: 1.6rem;
+  flex-shrink: 0;
 `
-const NavSection = ({ children }) => {
-  return <div></div>
+const StyledNavSection = styled.div`
+  margin-bottom: 2.8rem;
+  h2 {
+    font-weight: 500;
+    color: ${props => props.theme.colors.middlegrey};
+    font-size: 1.2rem;
+    text-transform: uppercase;
+    letter-spacing: 0.1rem;
+    margin-bottom: 1rem;
+    padding-left: 1.5rem;
+  }
+`
+const NavSection = ({ title, children }) => {
+  return (
+    <StyledNavSection>
+      {title && <h2>{title}</h2>}
+      {children}
+    </StyledNavSection>
+  )
 }
 const StyledNavItem = styled.div`
   display: flex;
+  height: 4rem;
+  padding: 0 1.2rem;
+  align-items: center;
+  &.spaced {
+    margin-bottom: 1.6rem;
+  }
+  .viz {
+    width: 4rem;
+    height: 4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 0.8rem;
+    border-radius: 1rem;
+    overflow: hidden;
+    .emoji {
+      width: 1.8rem;
+      height: 1.8rem;
+    }
+  }
+
+  p {
+    font-weight: 400;
+    color: ${props => props.theme.colors.grey};
+    &.highlighted {
+      color: ${props => props.theme.colors.dark};
+      font-size: 1.8rem;
+    }
+  }
 `
-const NavItem = ({ icon, text }) => {
+const NavItem = ({ icon, image, text, link = "/" }) => {
   return (
-    <StyledNavItem>
-      <img src={icon} alt="" />
-      <p>{text}</p>
-    </StyledNavItem>
+    <Link to={link} activeClassName="active">
+      <StyledNavItem className={image && "spaced"}>
+        <div className="viz">
+          {icon && <Emoji className="emoji" name={icon} />}
+          {image && <img src={image} />}
+        </div>
+        <p className={image && "highlighted"}>{text}</p>
+      </StyledNavItem>
+    </Link>
   )
 }
 const Navigation = () => {
   return (
     <StyledNavigation>
-      <ul>
-        <Link to="/">
-          <li> Home</li>
-        </Link>
-      </ul>
-      <ul>
-        <Link to="/scenarios">
-          <li>Scenarios</li>
-        </Link>
-      </ul>
       <NavSection>
-        <NavItem text="Scenarios" />
+        <NavItem image={ImageAcme} text="Acme Corp" link="/" />
+        <NavItem icon="telescope" text="Search" />
+        <NavItem icon="rocket" text="Updates" />
+      </NavSection>
+      <NavSection title="acme spaces">
+        <NavItem icon="telescope" text="Forecasting" />
+        <NavItem icon="rocket" text="Performance" />
+        <NavItem icon="chart-increasing" text="Scenarios" link="/scenarios" />
+      </NavSection>
+      <NavSection title="shared spaces">
+        <NavItem icon="telescope" text="Finance team" />
+        <NavItem icon="rocket" text="Investor dashboard" />
+      </NavSection>
+      <NavSection title="private spaces">
+        <NavItem icon="telescope" text="Notes" />
+      </NavSection>
+      <NavSection title="settings spaces">
+        <NavItem icon="telescope" text="Tables" />
+        <NavItem icon="rocket" text="Workflows" />
+        <NavItem icon="chart-increasing" text="Modeling" />
+        <NavItem icon="chart-increasing" text="Mappings" />
+        <NavItem icon="chart-increasing" text="Integrations" />
       </NavSection>
     </StyledNavigation>
   )
