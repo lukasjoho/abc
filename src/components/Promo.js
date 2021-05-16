ba07eba4635ca18487af55d70b59f07c0c718875
lukasjoho <social@lukashoppe.com>
2021-05-16 17:07:03 +0200

    Add hover state to promo component

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -2,12 +2,26 @@ import React from "react"
 import styled from "styled-components"
 import Emoji from "react-apple-emojis"
 import Button from "./Button"
+import ImageChart from "src/images/image-promo-chart.jpg"
+import ImageNavItem from "src/images/image-promo-navitem.jpg"
+import { Link } from "gatsby"
 const StyledPromo = styled.div`
   border: 1px solid #c9dbcb;
   border-radius: 1.5rem;
   background: #f5faf8;
   padding: 2rem;
-
+  margin-bottom: 4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  transition: 0.15s ease;
+  max-width: 900px;
+  &:hover {
+    border: 1px solid #9bb09d;
+  }
+  .text {
+    width: 50%;
+  }
   .label {
     display: flex;
     align-items: center;
@@ -28,20 +42,46 @@ const StyledPromo = styled.div`
     margin-bottom: 0.5rem;
   }
   p {
-    margin-bottom: 2rem;
+    margin-bottom: 1.5rem;
+  }
+  .images {
+    width: 50%;
+    position: relative;
+    .chart {
+      width: 100%;
+      border: 1px solid ${props => props.theme.colors.lightgrey};
+      border-radius: 1rem;
+    }
+    .navitem {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 50%;
+      transform: translate3d(-25%, 5%, 0);
+      border: 1px solid ${props => props.theme.colors.lightgrey};
+      border-radius: 1rem;
+    }
   }
 `
 const Promo = () => {
   return (
-    <StyledPromo>
-      <div className="label">
-        <Emoji className="emoji" name="sparkles" />
-        <h2>New!</h2>
-      </div>
-      <h3>Scenarios</h3>
-      <p>Let us introduce you to scenario models!</p>
-      <Button text="Get Started" />
-    </StyledPromo>
+    <Link to="/scenarios">
+      <StyledPromo>
+        <div classNae="text">
+          <div className="label">
+            <Emoji className="emoji" name="sparkles" />
+            <h2>New</h2>
+          </div>
+          <h3>Scenario Models</h3>
+          <p>Let us introduce you to scenario models! </p>
+          <Button text="Get Started" />
+        </div>
+        <div className="images">
+          <img className="chart" src={ImageChart} alt="" />
+          <img className="navitem" src={ImageNavItem} alt="" />
+        </div>
+      </StyledPromo>
+    </Link>
   )
 }
 
