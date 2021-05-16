3e4d728ecb2afcd6854113e2d4f5f91a799d0803
lukasjoho <social@lukashoppe.com>
2021-05-16 11:42:04 +0200

    Change font

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -6,9 +6,25 @@ html{
   -webkit-tap-highlight-color: transparent;
   body{
     font-size: 1.6rem;
-    font-family: "Helvetica";
+    font-family: "Inter";
     margin: 0;
     color: ${props => props.theme.colors.dark};
+    h1, h2,h3,h4,h5,h6{
+      margin: 0;
+    }
+    p{
+      margin: 0;
+    }
+    /* .active{
+      background: red;
+    } */
+    a{
+      text-decoration: none;
+      color: inherit;
+    }
+  }
+  *{
+    box-sizing: border-box;
   }
 }
 
