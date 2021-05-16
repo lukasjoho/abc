056cf7a8b594080534d5af02957ea27b23b4df83
lukasjoho <social@lukashoppe.com>
2021-05-16 11:46:44 +0200

    Change pagetitle emoji

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -1,10 +1,14 @@
 import React from "react"
 import styled from "styled-components"
-
+import Emoji from "react-apple-emojis"
 const StyledTitle = styled.div`
   display: flex;
-  img {
+  align-items: center;
+  margin-bottom: 4rem;
+  .emoji {
     height: 3rem;
+    width: 3rem;
+    margin-right: 2.2rem;
   }
   h1 {
     font-size: 3rem;
@@ -13,7 +17,7 @@ const StyledTitle = styled.div`
 const PageTitle = ({ icon, text }) => {
   return (
     <StyledTitle>
-      <img src={icon} alt="" />
+      <Emoji className="emoji" name={icon} />
       <h1>{text}</h1>
     </StyledTitle>
   )
